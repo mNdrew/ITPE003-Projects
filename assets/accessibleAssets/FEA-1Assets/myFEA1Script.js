115b9f0317951ddc4354e66ad812c6cef9bcda60
mNdrew <mandrew1425@gmail.com>
2023-07-22 17:46:34 +0800

    Update myFEA1Script.js

--- assets/accessibleAssets/FEA-1Assets/myFEA1Script.js
+++ assets/accessibleAssets/FEA-1Assets/myFEA1Script.js
@@ -1,63 +1,65 @@
-let cartAmount = 0;
-let cartPriceTotal = 0;
 
-let myOrder = {};
-const myCartItems = document.getElementById('myCartItems');
+        let cartAmount = 0;
+            let cartPriceTotal = 0;
 
-function addToCart(item, price){
-    if(myOrder[item] == undefined){
-        myOrder[item] = 1;
-    }else{
-        myOrder[item] += 1;
-    }
-    cartPriceTotal += price;
-    updateCartPrice();
-    updateCartAmount();
-    updateMyOrderList(item,price);      
-}
+            let myOrder = {};
+            const myCartItems = document.getElementById('myCartItems');
 
-function updateMyOrderList(item, price){
-    myCartItems.innerHTML = '';
-    for(let key in myOrder){
-        myCartItems.innerHTML += `
-        <div class="d-flex justify-content-between">
-        <div>${key}</div>
-        <div>${myOrder[key]}</div>
-        </div>`;
-    }
-    console.log(myOrder);
-}
+            function addToCart(item, price){
+                if(myOrder[item] == undefined){
+                    myOrder[item] = 1;
+                }else{
+                    myOrder[item] += 1;
+                }
+                cartPriceTotal += price;
+                updateCartPrice();
+                updateCartAmount();
+                updateMyOrderList(item,price);      
+            }
 
-function updateCartAmount(){
-    cartAmount += 1; 
-    console.log(cartAmount);
-    document.getElementById('cartItemNumber').innerHTML = cartAmount;
-}
+            function updateMyOrderList(item, price){
+                myCartItems.innerHTML = '';
+                for(let key in myOrder){
+                    myCartItems.innerHTML += `
+                    <div class="d-flex justify-content-between">
+                    <div>${key}</div>
+                    <div>${myOrder[key]}</div>
+                    </div>`;
+                }
+                console.log(myOrder);
+            }
 
-function updateCartPrice(){
-    document.getElementById('cartPrice').innerHTML = cartPriceTotal;
-}
+            function updateCartAmount(){
+                cartAmount += 1; 
+                console.log(cartAmount);
+                document.getElementById('cartItemNumber').innerHTML = cartAmount;
+            }
 
-function clearCart(){
-    myOrder = {};
-    cartAmount = 0;
-    cartPriceTotal = 0;
-    updateCartPrice();
-    updateCartAmount();
-    updateMyOrderList();
-}
+            function updateCartPrice(){
+                document.getElementById('cartPrice').innerHTML = cartPriceTotal;
+            }
+            
+            function clearCart(){
+                myOrder = {};
+                cartAmount = 0;
+                cartPriceTotal = 0;
+                updateCartPrice();
+                updateCartAmount();
+                updateMyOrderList();
+            }
 
-function resetProgram(){
-    location.reload();
-}
+            function resetProgram(){
+                location.reload();
+            }
 
-function checkOut(){
-    if(cartPriceTotal == 0){
-        alert("Please add items to your cart!");
-        console.log("No items in cart!");
-        return;
-    }
-    alert("Thank you for your purchase!"+"\nYour total amount is ₱" + cartPriceTotal);
-    console.log("Thank you for your purchase!"+"\nYour total amount is ₱" + cartPriceTotal)
-    resetProgram();
-}
+            function checkOut(){
+                if(cartPriceTotal == 0){
+                    alert("Please add items to your cart!");
+                    console.log("No items in cart!");
+                    return;
+                }
+                alert("Thank you for your purchase!"+"\nYour total amount is ₱" + cartPriceTotal);
+                console.log("Thank you for your purchase!"+"\nYour total amount is ₱" + cartPriceTotal)
+                resetProgram();
+            }
+   
